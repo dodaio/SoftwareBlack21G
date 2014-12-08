1faf5b7a52411abc03283a4f7decb3f6e9b71acc
dodaio <dodaio@gmail.com>
2014-12-09 01:21:37 +0200

    Hit stand and deal buttons update the lable please press them and you will see changes

--- Blackjack/src/Viewer/MainGameController.java
+++ Blackjack/src/Viewer/MainGameController.java
@@ -6,8 +6,10 @@ package Viewer;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
@@ -31,6 +33,9 @@ public class MainGameController extends AnchorPane implements Initializable {
 	@FXML
 	TextField txt_Pcard3;
 	
+	@FXML
+	Label Massegelbl;
+	
 	
 	
 	
@@ -45,6 +50,31 @@ public class MainGameController extends AnchorPane implements Initializable {
 		txt_Pcard2.setText("Empty");
 		txt_Pcard3.setText("Empty");
 		
+		Massegelbl.setText("Whoooooo");
+		
 	}
+	
+	
+	
+	@FXML
+	private void Exit(){
+		System.exit(0);
+	}
+	
+	@FXML
+	private void HandleHit(ActionEvent ex){
+		Massegelbl.setText("Hit");
+	}
+	
+	@FXML
+	private void HandleDeal(ActionEvent ex){
+		Massegelbl.setText("Deal");
+	}
+	
+	@FXML
+	private void HandleStand(ActionEvent ex){
+		Massegelbl.setText("Stand");
+	}
+	
 
 }
