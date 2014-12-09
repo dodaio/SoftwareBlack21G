b3b3c330a2eeeb464eff08e47b9c1320c365f1c6
dodaio <dodaio@gmail.com>
2014-12-09 20:28:39 +0200

    Updated methods - acoording to uml.
    changed from txtfield to lblfields

--- Blackjack/src/Viewer/MainGameController.java
+++ Blackjack/src/Viewer/MainGameController.java
@@ -6,11 +6,9 @@ package Viewer;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -19,36 +17,58 @@ import javafx.scene.layout.AnchorPane;
  */
 public class MainGameController extends AnchorPane implements Initializable {
 
-	@FXML
-	TextField txt_Dcard1;
-	@FXML
-	TextField txt_Dcard2;
-	@FXML
-	TextField txt_Dcard3;
-	
-	@FXML
-	TextField txt_Pcard1;
-	@FXML
-	TextField txt_Pcard2;
-	@FXML
-	TextField txt_Pcard3;
+//	@FXML
+//	TextField txt_Dcard1;
+//	@FXML
+//	TextField txt_Dcard2;
+//	@FXML
+//	TextField txt_Dcard3;
+//	
+//	@FXML
+//	TextField txt_Pcard1;
+//	@FXML
+//	TextField txt_Pcard2;
+//	@FXML
+//	TextField txt_Pcard3;
 	
 	@FXML
 	Label Massegelbl;
 	
+	//*******************************Dealer variables************************************
+	@FXML
+	Label MassegelblD1;
+	@FXML
+	Label MassegelblD2;
+	@FXML
+	Label MassegelblD3;
+	@FXML
+	Label lblHeaderD3;
+	
+	//*******************************Player variables************************************
+	
+	@FXML
+	Label Massegelblp1;
+	@FXML
+	Label Massegelblp2;
+	@FXML
+	Label Massegelblp3;
+	@FXML
+	Label lblHeaderP3;
 	
 	
 	
+	//*******************************Initialize************************************
+
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		txt_Dcard1.setText("Hidden");
-		txt_Dcard2.setText("Some Card");
-		txt_Dcard3.setText("None");
+		MassegelblD1.setText("Hidden");
+		MassegelblD2.setText("Some Card");
+		//MassegelblD3.setText("None");
 		
-		txt_Pcard1.setText("Empty");
-		txt_Pcard2.setText("Empty");
-		txt_Pcard3.setText("Empty");
+		Massegelblp1.setText("Empty");
+		Massegelblp2.setText("Empty");
+		//Massegelblp3.setText("Empty");
 		
 		Massegelbl.setText("Whoooooo");
 		
@@ -62,17 +82,17 @@ public class MainGameController extends AnchorPane implements Initializable {
 	}
 	
 	@FXML
-	private void HandleHit(ActionEvent ex){
+	private void onHitClick(){
 		Massegelbl.setText("Hit");
 	}
 	
 	@FXML
-	private void HandleDeal(ActionEvent ex){
+	private void onDealClick(){
 		Massegelbl.setText("Deal");
 	}
 	
 	@FXML
-	private void HandleStand(ActionEvent ex){
+	private void onStandClick(){
 		Massegelbl.setText("Stand");
 	}
 	
